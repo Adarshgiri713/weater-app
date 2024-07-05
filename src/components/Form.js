87edcb44dbb066a033f628ebcb3366f75634680a
Adarsh giri <adarshgiri91@gmail.com>
2024-07-05 09:21:51 -0700

    Second commit

--- src/components/Form.js
+++ src/components/Form.js
@@ -5,16 +5,40 @@ import { getWeather } from '../services/api';
 const Container = styled(Box)({
   background: "#445A6F",
   padding: 10,
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  textAlign: "center",
+  "@media (min-width: 600px)": {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
 });
 
-const Input = styled(InputBase)({
+const Input = styled(InputBase)(({ theme }) => ({
   color: "#FFF",
+  margin: theme.spacing(1),
   marginRight: 20,
-  fontSize: 18,
-});
+  marginBottom: 20,
+  "& input": {
+    backgroundColor: "rgba(255, 255, 255, 0.1)",
+    padding: "10px 12px",
+    borderRadius: 5,
+    minWidth: 200,
+    "&:focus": {
+      backgroundColor: "rgba(255, 255, 255, 0.2)",
+    },
+  },
+  "@media (min-width: 600px)": {
+    marginBottom: 0,
+  },
+}));
 
 const GetButton = styled(Button)({
   background: "#e67e22",
+  marginTop: 20,
+  minWidth: 150,
 });
 
 const Form = ({ setResult }) => {
